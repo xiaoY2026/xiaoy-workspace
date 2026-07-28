82de9539ae39165daca89b71f5a56705b6ec9117
xiaoy <xiaoy@local>
2026-07-28 16:28:55 +0800

    小Y工作台 v6: 自定义背景/图标、体重记录、每日刷新、英语口语跟读

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'xiaoy-v4';
+const ASSETS = ['./', './index.html', './app.js', './manifest.json', './bg.jpg', './apple-touch-icon.png', './icon-192.png', './icon-512.png', './news.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => {
+      if (r) return r;
+      return fetch(e.request).then(res => {
+        if (res.ok && res.type === 'basic') {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
